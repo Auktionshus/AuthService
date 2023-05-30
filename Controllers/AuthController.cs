588f9e39878b6591f514c96e2a1065e6ae433db6
Michael H Sloth <michael.h.sloth@gmmail.com>
2023-05-30 09:56:18 +0200

    SomantiskVersioneringTilføjet

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -150,5 +150,18 @@ namespace AuthService.Controllers
         {
             return Ok("You're authorized");
         }
+
+        [HttpGet("version")]
+        public IEnumerable<string> Get()
+        {
+            var properties = new List<string>();
+            var assembly = typeof(Program).Assembly;
+            foreach (var attribute in assembly.GetCustomAttributesData())
+            {
+                properties.Add($"{attribute.AttributeType.Name} - {attribute.ToString()}");
+            }
+            return properties;
+
+        }
     }
 }
